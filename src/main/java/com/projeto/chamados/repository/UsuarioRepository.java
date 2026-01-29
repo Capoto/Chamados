38a8254e1e8450d5d341295c0b72fb8736c79518
Capoto <gomespereiraheitor@gmail.com>
2026-01-29 09:21:49 -0300

    Refatorando as pastas e colocando camada de segurança

--- src/main/java/com/projeto/chamados/repository/UsuarioRepository.java
+++ src/main/java/com/projeto/chamados/repository/UsuarioRepository.java
@@ -2,7 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.projeto.chamados.data;
+package com.projeto.chamados.repository;
+import com.projeto.chamados.data.UsuarioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 /**
